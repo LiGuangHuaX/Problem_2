0c1c5d9d28db5ccf8c58b4c0103e3bd363f91c12
Rubbly <iRubbly@outlook.com>
2018-07-20 22:42:11 +0800

    first commit

---
+++ Assets/Scenes/Enemy.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[AddComponentMenu("MyGame/Enemy")]
+public class Enemy : MonoBehaviour
+{
+    private new Transform transform;
+    private Animator animator; // 动画组件
+    private Player player; // 主角
+    private NavMeshAgent agent; // 寻路组件
+    private float moveSpeed = 1.5f; // 角色移动速度
+    private float rotateSpeed = 30; // 角色旋转速度
+    private float timer = 2; // 计时器
+    private int life = 10; // 生命值
+
+    void Awake() { 
+}
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        // 获取动画组件
+        animator = GetComponent<Animator>();
+        // 获取主角
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();  //
+        // 获取寻路组件
+        
+            agent = GetComponent<NavMeshAgent>();
+        
+        // 指定寻路器的行走速度
+        agent.speed = moveSpeed;
+    }
+    /// <summary>
+        /// 转向目标方向
+        /// </summary>
+    private void RotateTo()
+    {
+        // 获取目标方向
+        Vector3 targetDirection = player.transform.position - transform.position;
+        // 计算旋转角度
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward,
+ targetDirection, rotateSpeed * Time.deltaTime, 0);
+        // 旋转至新方向
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+       /// <summary>
+    /// 更新敌人行为
+    /// </summary>
+    void Update()
+    {
+        // 如果主角生命为0，则什么也不做
+        if (player.life <= 0)
+        {
+            return;
+        }     // 获取当前动画状态
+               AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);       // 待机状态
+
+ if (info.fullPathHash == Animator.StringToHash("Base Layer.idle")
+ && !animator.IsInTransition(0))
+        {
+            animator.SetBool("idle", false);
+            // 待机一定时间
+            timer -= Time.deltaTime;
+            if (timer > 0)
+            {
+                return;
+            }
+            // 如果距离主角1.5米以内，则进入攻击状态
+            if (Vector3.Distance(transform.position, player.transform.position) < 1.5f)
+            {
+                animator.SetBool("attack", true);
+            }
+            else
+            {
+                // 重置定时器
+                timer = 1;
+                // 恢复寻路
+                agent.Resume();
+                // 设置寻路目标
+                agent.SetDestination(player.transform.position);
+                // 面向主角
+                RotateTo();
+                Debug.Log(agent.destination);
+                // 进入行走状态
+                animator.SetBool("run", true);
+            }
+        }    // 行走状态
+
+ if (info.fullPathHash == Animator.StringToHash("Base Layer.run")
+ && !animator.IsInTransition(0))
+        {
+            animator.SetBool("run", false);
+            // 每隔1秒重新定位主角的位置
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                timer = 1;
+                // 面向主角
+                RotateTo();
+                agent.SetDestination(player.transform.position);
+            }
+            // 如果距离主角1.5米以内，则进攻主角
+            if (Vector3.Distance(transform.position, player.transform.position) < 1.5f)
+            {
+                // 停止寻路
+                agent.Stop();
+                // 进入攻击状态
+                animator.SetBool("attack", true);
+            }
+        }      // 攻击状态
+
+ if (info.fullPathHash == Animator.StringToHash("Base Layer.attack")
+ && !animator.IsInTransition(0))
+        {
+            animator.SetBool("attack", false);
+            // 面向主角
+            RotateTo();
+            // 如果动画播放完，重新进入待机状态
+            if (info.normalizedTime >= 1)
+            {
+                animator.SetBool("idle", true);
+                // 重置计时器
+                timer = 2;
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[AddComponentMenu("MyGame/Player")] // 这一句的作用是把该脚本放到【Component】→【MyGame】下的【Player】，便于自行管理脚本
+public class Player : MonoBehaviour // 需要由Unity控制生命周期的脚本都要继承MonoBehaviour类，并且要挂在一个游戏体上
+{
+    public int life = 10;
+
+    private new Transform transform; // 此处使用new关键字隐藏父类的同名成员
+    private CharacterController controller;
+    private float speed = 3.0f;
+    private float gravity = 2.0f;
+
+    private Transform cameraTransform; // 摄像机的Transform组件
+    private Vector3 cameraRotation; // 摄像机旋转角度
+    private float cameraHeight = 1.7f; // 摄像机高度（即主角的眼睛高度）
+
+
+    /// <summary>
+    /// 初始化,获取Transform和Character Controller组件
+    /// </summary>
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
+        // 获取摄像机
+        cameraTransform = Camera.main.GetComponent<Transform>();
+        // 设置摄像机初始位置
+        Vector3 position = transform.position;
+        position.y += cameraHeight;
+        cameraTransform.position = position;
+        // 设置摄像机的初始旋转方向
+        cameraTransform.rotation = transform.rotation;
+        cameraRotation = cameraTransform.eulerAngles;
+        // 锁定鼠标
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    /// <summary>
+    /// 控制主角行动，如果生命为0则什么也不做
+    /// </summary>
+    void Update()
+    {
+        if (life <= 0)
+        {
+            return;
+        }
+        Control();
+    }
+
+    /// <summary>
+    /// 在Unity编辑器中为主角显示一个图标
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(GetComponent<Transform>().position, "Spawn.tif");
+    }
+
+    /// <summary>
+    /// 控制主角的重力运动和前后左右移动
+    /// </summary>
+    private void Control()
+    {
+
+        float x = 0, y = 0, z = 0;
+        // 重力运动
+        y -= gravity * Time.deltaTime;
+        // 前后移动
+        if (Input.GetKey(KeyCode.W))
+        {
+            z += speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            z -= speed * Time.deltaTime;
+        }
+        // 左右移动
+        if (Input.GetKey(KeyCode.A))
+        {
+            x -= speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            x += speed * Time.deltaTime;
+        }
+
+        // 使用Character Controller而不是Transform提供的Move方法
+        // 因为Character Controller提供的Move方法会自动进行碰撞检测
+        controller.Move(transform.TransformDirection(new Vector3(x, y, z)));
+
+      // 获取鼠标移动距离
+        float rh = Input.GetAxis("Mouse X");
+        float rv = Input.GetAxis("Mouse Y");
+        // 旋转摄像机
+        cameraRotation.x -= rv;
+        cameraRotation.y += rh;
+        cameraTransform.eulerAngles = cameraRotation;
+        // 使主角的面向方向与摄像机一致
+        Vector3 rotation = cameraTransform.eulerAngles;
+        rotation.x = 0;
+        rotation.z = 0;
+        transform.eulerAngles = rotation;
+
+        // 控制主角运动
+
+        // 使摄像机的位置与主角一致
+        Vector3 position = transform.position;
+        position.y += cameraHeight;
+        cameraTransform.position = position;
+    }
+
+
+
+}
+
